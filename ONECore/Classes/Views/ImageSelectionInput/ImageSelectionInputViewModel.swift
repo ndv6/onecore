b8063a21a1678b53c6d55c9d9f9b848de0988c90
fradenza <denza.dev@gmail.com>
2020-01-20 17:22:45 +0700

    fix ImageSelectionInputViewModel

--- ONECore/Classes/Views/ImageSelectionInput/ImageSelectionInputViewModel.swift
+++ ONECore/Classes/Views/ImageSelectionInput/ImageSelectionInputViewModel.swift
@@ -56,5 +56,10 @@ public class ImageSelectionInputViewModel {
 >>>>>>> fix linter error
 =======
     public var didChangeAction: InputDidChangeHandler = { _, _  in }
+<<<<<<< HEAD
 >>>>>>> fix 70 warnings
+=======
+
+    public init() {}
+>>>>>>> fix ImageSelectionInputViewModel
 }
