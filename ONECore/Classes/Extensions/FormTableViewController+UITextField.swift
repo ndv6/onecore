aa1a08b18d80e609c76030e9e3431043cd397683
Aldo <aldo.onelabs@gmail.com>
2020-01-21 11:12:24 +0700

    fix replacement string

--- ONECore/Classes/Extensions/FormTableViewController+UITextField.swift
+++ ONECore/Classes/Extensions/FormTableViewController+UITextField.swift
@@ -90,6 +90,7 @@ extension FormTableViewController: UITextFieldDelegate {
         var result = isValidLength && txtField.shouldChangeCharactersIn(range: range, replacementString: string)
         if !result && string.isBackspace() { return true }
         if !result { return false }
+<<<<<<< HEAD
         var replacementString = string
         if txtField.autocapitalizationType == .allCharacters {
             replacementString = replacementString.uppercased()
@@ -108,6 +109,10 @@ extension FormTableViewController: UITextFieldDelegate {
             result = false
         }
 >>>>>>> revert code
+=======
+        var replacementString = composeReplacementStringFrom(string, textfield: txtField)
+        result = !isNeedToOverrideText(textfield: txtField)
+>>>>>>> fix replacement string
         let updatedText = getUpdatedText(
             textField,
             shouldChangeCharactersIn: range,
