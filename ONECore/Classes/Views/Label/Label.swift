ed8cf7df641bfc916f458af535214a2a8e3d565e
Steven Tio <steventio2011@gmail.com>
2020-02-03 03:32:45 +0700

    improve label function

--- ONECore/Classes/Views/Label/Label.swift
+++ ONECore/Classes/Views/Label/Label.swift
@@ -20,4 +20,34 @@ open class Label: UILabel {
         textColor = style.textColor
         textAlignment = style.alignment
     }
+
+    public func setTextWithPartialHighlight(
+        fullText: String,
+        highlightText: String = DefaultValue.emptyString,
+        highlightFont: UIFont,
+        highlightColor: UIColor? = nil
+    ) {
+        let attribute = NSMutableAttributedString(string: fullText)
+        let highlightRange = NSRange(
+            location: fullText.count - highlightText.count,
+            length: highlightText.count
+        )
+        attribute.addAttribute(
+            NSAttributedString.Key.font,
+            value: highlightFont,
+            range: highlightRange
+        )
+        attribute.addAttribute(
+            NSAttributedString.Key.foregroundColor,
+            value: highlightColor ?? UIColor.black,
+            range: NSRange(location: 0, length: fullText.count)
+        )
+        attribute.addAttribute(
+            NSAttributedString.Key.foregroundColor,
+            value: highlightColor ?? UIColor.black,
+            range: highlightRange
+        )
+        self.attributedText = attribute
+//        setAttributedTitle(attribute, for: .normal)
+    }
 }
--- ONECore/Classes/Views/PinInput/PinInputView.swift
+++ ONECore/Classes/Views/PinInput/PinInputView.swift
@@ -144,8 +144,8 @@ open class PinInputView: UIView {
         if containerSize.width == DefaultValue.emptyCGFloat { return }
         removeAllSubviews()
         renderTextField()
-        renderPanViews(containerSize: containerSize)
-        renderKeyboardButton(containerSize: containerSize)
+        renderPanViews(containerSize: CGSize(width: 320, height: 64))
+        renderKeyboardButton(containerSize: CGSize(width: 320, height: 64))
         didChangedValue()
     }
 
